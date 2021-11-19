99c4b29c9b0280032353939fd31560e646f1af08
marrrkkk <definitelynotmark13@gmail.com>
2021-11-19 21:14:31 +0800

    Added admin check

--- commands/dev/guildinfo.js
+++ commands/dev/guildinfo.js
@@ -14,16 +14,23 @@ module.exports = {
     run: async(client, message, args) => {
         if(message.author.id !== ownerID) return
         try {
-            const guild = client.guilds.cache.get(args[0]) || client.guilds.cache.find(g => g.name.toLowerCase() === args.join(' '))
-            if(!args[0]) return await message.channel.send('<:cross:893700435616100402> Provide a Server ID')
+            let guild = client.guilds.cache.get(args[0]) || client.guilds.cache.find(g => g.name.toLowerCase() === args.join(' '))
+            if(!args[0]) guild = message.guild
             if(!guild) return await message.channel.send(`<:cross:893700435616100402> Unable to find server "${args[0]}"`)
 
             const owner = guild.members.cache.get(guild.ownerId)
 
+            let admin
+            if(guild.me.permissions.has('ADMINISTRATOR')){
+                admin = 'Yes'
+            } else {
+                admin = 'No'
+            }
+
             const embed = new MessageEmbed()
             .setAuthor(guild.name, guild.iconURL())
             .setThumbnail(guild.iconURL() || 'https://cdn.discordapp.com/embed/avatars/0.png')
-            .setDescription(`**Owner**\n${owner.user.tag}\n\n**Verification:** ${guild.verificationLevel}\n**Boost:** ${guild.premiumSubscriptionCount}`)
+            .setDescription(`**Owner**\n${owner.user.tag}\n\n**Verification:** ${guild.verificationLevel}\n**Boost:** ${guild.premiumSubscriptionCount}\n**Admin:** ${admin}`)
             .addField('Stats', `\`\`\`ini\nText Channel:   [${guild.channels.cache.filter(c => c.type === 'GUILD_TEXT').size}]\nVoice Channel:  [${guild.channels.cache.filter(c => c.type === 'GUILD_VOICE').size}]\nUsers:          [${guild.members.cache.filter(m => !m.user.bot).size}]\nBots:           [${guild.members.cache.filter(m => m.user.bot).size}]\nTotal Channels: [${guild.channels.cache.size}]\nTotal Members:  [${guild.memberCount}]\n\`\`\``)
             .setFooter(`ID: ${guild.id}`)
             .setColor('#2f3136')
