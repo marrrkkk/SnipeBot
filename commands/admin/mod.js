a9d5acccfc2ac05e67c3fe4affb063d7d688679a
marrrkkk <definitelynotmark13@gmail.com>
2021-11-26 20:45:00 +0800

    Fixed bugs

--- commands/admin/mod.js
+++ commands/admin/mod.js
@@ -1,4 +1,5 @@
 const { Client, Message, MessageActionRow, MessageButton, MessageEmbed, Permissions } = require('discord.js')
+const { emojis } = require('../../config.json')
 const moment = require('moment')
 
 module.exports = {
@@ -32,13 +33,22 @@ module.exports = {
             .setLabel('Ban')
             .setStyle('DANGER')
         )
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        let Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) ||
+        message.guild.members.cache.find(m => m.user.username === args.join(' ')) || message.guild.members.cache.find(m => m.user.username.toLowerCase() === args.join(' ')) ||
+        message.guild.members.cache.find(m => m.displayName === args.join(' ')) || message.guild.members.cache.find(m => m.displayName.toLowerCase() === args.join(' '))
+        if(!args[0]) Member = message.member
+
+        const err = new MessageEmbed()
+        .setDescription(`${emojis.cross} Could't find member "**${args.join(' ')}**"`)
+        .setColor('RED')
+        
+        if(!Member) return message.channel.send({ embeds: [err] })
 
         const embed = new MessageEmbed()
-        .setAuthor(member.user.tag, member.user.displayAvatarURL())
-        .setDescription(`<@${member.id}>`)
-        .addField('Joined Server', `${moment(member.joinedAt).format("LL")}`, true)
-        .addField('Created Account', `${moment(member.user.createdAt).format("LL")}`, true)
+        .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
+        .setDescription(`<@${Member.id}>`)
+        .addField('Joined Server', `${moment(Member.joinedAt).format("LL")}`, true)
+        .addField('Created Account', `${moment(Member.user.createdAt).format("LL")}`, true)
         .setColor('BLURPLE')
 
         let msg = await message.channel.send({ embeds: [embed], components: [row] }).catch(e => console.log(e))
@@ -53,37 +63,37 @@ module.exports = {
                 row.components[1].setDisabled(true)
 
                 const fail1 = new MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
+                .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
                 .setDescription('<:cross:890077548799725619> Unable to kick that member')
                 .setColor('RED')
 
                 const fail2 = new MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
+                .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
                 .setDescription('<:cross:890077548799725619> Unable to kick someone higher or the same role as you')
                 .setColor('RED')
 
                 const fail3 = new MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
+                .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
                 .setDescription("<:cross:890077548799725619> You don't have permission to kick members")
                 .setColor('RED')
 
                 const fail4 = new MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
+                .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
                 .setDescription("<:cross:890077548799725619> I don't have permission to kick members")
                 .setColor('RED')
 
                 const success = new MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
-                .setDescription(`Kicked **${member.user.username}**`)
+                .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
+                .setDescription(`Kicked **${Member.user.username}**`)
                 .setColor('GREEN')
 
                 if(!message.guild.me.permissions.has('KICK_MEMBERS')) return await msg.edit({ embeds: [fail4], components: [row] })
                 if(!message.member.permissions.has('KICK_MEMBERS')) return await msg.edit({ embeds: [fail3], components: [row] })
-                if(!member.kickable) return await msg.edit({ embeds: [fail1], components: [row] })
-                if(message.member.roles.highest.position <= member.roles.highest.position) return await msg.edit({ embeds: [fail2], components: [row] })
+                if(!Member.kickable) return await msg.edit({ embeds: [fail1], components: [row] })
+                if(message.member.roles.highest.position <= Member.roles.highest.position) return await msg.edit({ embeds: [fail2], components: [row] })
         
-                await member.send(`You have been kicked from ${message.guild.name}`).catch(e => console.log(e))
-                await member.kick().catch(e => console.log(e)).then(() => msg.edit({ embeds: [success], components: [row] }))
+                await Member.send(`You have been kicked from ${message.guild.name}`).catch(e => console.log(e))
+                await Member.kick().catch(e => console.log(e)).then(() => msg.edit({ embeds: [success], components: [row] }))
             }
 
             if(b.customId === 'ban'){
@@ -91,37 +101,37 @@ module.exports = {
                 row.components[1].setDisabled(true)
 
                 const fail1 = new MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
+                .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
                 .setDescription('<:cross:890077548799725619> Unable to ban that member')
                 .setColor('RED')
 
                 const fail2 = new MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
+                .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
                 .setDescription('<:cross:890077548799725619> Unable to ban someone higher or the same role as you')
                 .setColor('RED')
 
                 const fail3 = new MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
+                .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
                 .setDescription("<:cross:890077548799725619> You don't have permission to ban members")
                 .setColor('RED')
 
                 const fail4 = new MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
+                .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
                 .setDescription("<:cross:890077548799725619> I don't have permission to ban members")
                 .setColor('RED')
 
                 const success = new MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
-                .setDescription(`Banned **${member.user.username}**`)
+                .setAuthor(Member.user.tag, Member.user.displayAvatarURL())
+                .setDescription(`Banned **${Member.user.username}**`)
                 .setColor('GREEN')
 
                 if(!message.guild.me.permissions.has('BAN_MEMBERS')) return await msg.edit({ embeds: [fail4], components: [row] })
                 if(!message.member.permissions.has('BAN_MEMBERS')) return await msg.edit({ embeds: [fail3], components: [row] })
-                if(!member.bannable) return await msg.edit({ embeds: [fail1], components: [row] })
-                if(message.member.roles.highest.position <= member.roles.highest.position) return await msg.edit({ embeds: [fail2], components: [row] })
+                if(!Member.bannable) return await msg.edit({ embeds: [fail1], components: [row] })
+                if(message.member.roles.highest.position <= Member.roles.highest.position) return await msg.edit({ embeds: [fail2], components: [row] })
         
-                await member.send(`You have been banned from ${message.guild.name}`).catch(e => console.log(e))
-                await member.ban({ days: 7 }).catch(e => console.log(e)).then(() => msg.edit({ embeds: [success], components: [row] }))
+                await Member.send(`You have been banned from ${message.guild.name}`).catch(e => console.log(e))
+                await Member.ban({ days: 7 }).catch(e => console.log(e)).then(() => msg.edit({ embeds: [success], components: [row] }))
             }
         })
 
