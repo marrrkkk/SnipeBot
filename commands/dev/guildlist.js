b6fbc3d512bce91f6a07f57ced90c5aa53cb15a5
marrrkkk <definitelynotmark13@gmail.com>
2022-01-18 08:27:26 +0800

    Improved `guildlist.js`

--- commands/dev/guildlist.js
+++ commands/dev/guildlist.js
@@ -28,83 +28,263 @@ module.exports = {
             .setEmoji(emojis.right)
             .setStyle('PRIMARY')
         )
+        .addComponents(
+          new MessageButton()
+          .setLabel('Leave')
+          .setCustomId('leave')
+          .setStyle('DANGER')
+        )
 
-        let i0 = 0
-        let i1 = 10
-        let page = 1
+        if(!args[0]){
+          let i0 = 0
+          let i1 = 1
+          let page = 1
+  
+          let description =
+          `Total Servers - ${client.guilds.cache.size}\n\n` +
+          client.guilds.cache
+            .sort((a, b) => b.memberCount - a.memberCount)
+            .map(r => r)
+            .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
+            .slice(0, 1)
+            .join("\n\n");
+  
+        const embed = new MessageEmbed()
+          .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
+          .setColor(color)
+          .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 1)}`)
+          .setDescription(description);
+  
+          let msg = await message.channel.send({ embeds: [embed], components: [row] });
+          const collector = msg.createMessageComponentCollector({ componentType: 'BUTTON'})
+  
+        collector.on('collect', async b => {
+          b.deferUpdate()
+          if(b.user.id !== message.author.id) return
+          if(b.customId === 'left'){
+  
+              if(page <= 1) return
+              if(page > Math.ceil(client.guilds.cache.size / 1)) return
+  
+              i0 = i0 - 1
+              i1 = i1 - 1
+              page = page - 1
+  
+              let description1 =
+              `Total Servers - ${client.guilds.cache.size}\n\n` +
+              client.guilds.cache
+                .sort((a, b) => b.memberCount - a.memberCount)
+                .map(r => r)
+                .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
+                .slice(i0, i1)
+                .join("\n\n");
+  
+              const embed0 = new MessageEmbed()
+              .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
+              .setColor(color)
+              .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 1)}`)
+              .setDescription(description1);
+  
+              return await msg.edit({ embeds: [embed0] })
+          }
+  
+          if(b.customId === 'right'){
+            if(page >= Math.ceil(client.guilds.cache.size / 1)) return
+  
+              i0 = i0 + 1
+              i1 = i1 + 1
+              page = page + 1
+  
+              let description2 = 
+              `Total Servers - ${client.guilds.cache.size}\n\n` +
+              client.guilds.cache
+                .sort((a, b) => b.memberCount - a.memberCount)
+                .map(r => r)
+                .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
+                .slice(i0, i1)
+                .join("\n\n");
+  
+              const embed1 = new MessageEmbed()
+              .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
+              .setColor(color)
+              .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 1)}`)
+              .setDescription(description2);
+  
+              return await msg.edit({ embeds: [embed1] })
+          }
 
-        let description =
-        `Total Servers - ${client.guilds.cache.size}\n\n` +
-        client.guilds.cache
-          .sort((a, b) => b.memberCount - a.memberCount)
-          .map(r => r)
-          .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
-          .slice(0, 10)
-          .join("\n\n");
+          if(b.customId === 'leave'){
 
-      const embed = new MessageEmbed()
-        .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
-        .setColor(color)
-        .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
-        .setDescription(description);
-
-        let msg = await message.channel.send({ embeds: [embed], components: [row] });
-        const collector = msg.createMessageComponentCollector({ componentType: 'BUTTON'})
-
-      collector.on('collect', async b => {
-        b.deferUpdate()
-        if(b.user.id !== message.author.id) return
-        if(b.customId === 'left'){
-
-            if(page <= 1) return
-            if(page > Math.ceil(client.guilds.cache.size / 10)) return
-
-            i0 = i0 - 10
-            i1 = i1 - 10
-            page = page - 1
-
-            let description1 =
-            `Total Servers - ${client.guilds.cache.size}\n\n` +
+            let description3 = 
             client.guilds.cache
               .sort((a, b) => b.memberCount - a.memberCount)
               .map(r => r)
-              .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
+              .map((r) => `${r.id}`)
               .slice(i0, i1)
               .join("\n\n");
 
-            const embed0 = new MessageEmbed()
-            .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
-            .setColor(color)
-            .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
-            .setDescription(description1);
+            const guild = client.guilds.cache.get(description3)
 
-            return await msg.edit({ embeds: [embed0] })
+            await message.channel.send(`Leave **${guild.name}**`)
+            return await guild.leave()
+          }
+        })
+        } else if (args[0] === '--lowest'){
+          let i0 = 0
+          let i1 = 10
+          let page = 1
+  
+          let description =
+          `Total Servers - ${client.guilds.cache.size}\n\n` +
+          client.guilds.cache
+            .sort((a, b) => a.memberCount - b.memberCount)
+            .map(r => r)
+            .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
+            .slice(0, 10)
+            .join("\n\n");
+  
+        const embed = new MessageEmbed()
+          .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
+          .setColor(color)
+          .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
+          .setDescription(description);
+  
+          let msg = await message.channel.send({ embeds: [embed], components: [row] });
+          const collector = msg.createMessageComponentCollector({ componentType: 'BUTTON'})
+  
+        collector.on('collect', async b => {
+          b.deferUpdate()
+          if(b.user.id !== message.author.id) return
+          if(b.customId === 'left'){
+  
+              if(page <= 1) return
+              if(page > Math.ceil(client.guilds.cache.size / 10)) return
+  
+              i0 = i0 - 10
+              i1 = i1 - 10
+              page = page - 1
+  
+              let description1 =
+              `Total Servers - ${client.guilds.cache.size}\n\n` +
+              client.guilds.cache
+                .sort((a, b) => a.memberCount - b.memberCount)
+                .map(r => r)
+                .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
+                .slice(i0, i1)
+                .join("\n\n");
+  
+              const embed0 = new MessageEmbed()
+              .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
+              .setColor(color)
+              .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
+              .setDescription(description1);
+  
+              return await msg.edit({ embeds: [embed0] })
+          }
+  
+          if(b.customId === 'right'){
+            if(page >= Math.ceil(client.guilds.cache.size / 10)) return
+  
+              i0 = i0 + 10
+              i1 = i1 + 10
+              page = page + 1
+  
+              let description2 = 
+              `Total Servers - ${client.guilds.cache.size}\n\n` +
+              client.guilds.cache
+                .sort((a, b) => a.memberCount - b.memberCount)
+                .map(r => r)
+                .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
+                .slice(i0, i1)
+                .join("\n\n");
+  
+              const embed1 = new MessageEmbed()
+              .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
+              .setColor(color)
+              .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
+              .setDescription(description2);
+  
+              return await msg.edit({ embeds: [embed1] })
+          }
+        })
+        } else if (args[0] === '--list'){
+          let i0 = 0
+          let i1 = 10
+          let page = 1
+  
+          let description =
+          `Total Servers - ${client.guilds.cache.size}\n\n` +
+          client.guilds.cache
+            .sort((a, b) => b.memberCount - a.memberCount)
+            .map(r => r)
+            .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
+            .slice(0, 10)
+            .join("\n\n");
+  
+        const embed = new MessageEmbed()
+          .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
+          .setColor(color)
+          .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
+          .setDescription(description);
+  
+          let msg = await message.channel.send({ embeds: [embed], components: [row] });
+          const collector = msg.createMessageComponentCollector({ componentType: 'BUTTON'})
+  
+        collector.on('collect', async b => {
+          b.deferUpdate()
+          if(b.user.id !== message.author.id) return
+          if(b.customId === 'left'){
+  
+              if(page <= 1) return
+              if(page > Math.ceil(client.guilds.cache.size / 10)) return
+  
+              i0 = i0 - 10
+              i1 = i1 - 10
+              page = page - 1
+  
+              let description1 =
+              `Total Servers - ${client.guilds.cache.size}\n\n` +
+              client.guilds.cache
+                .sort((a, b) => b.memberCount - a.memberCount)
+                .map(r => r)
+                .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
+                .slice(i0, i1)
+                .join("\n\n");
+  
+              const embed0 = new MessageEmbed()
+              .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
+              .setColor(color)
+              .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
+              .setDescription(description1);
+  
+              return await msg.edit({ embeds: [embed0] })
+          }
+  
+          if(b.customId === 'right'){
+            if(page >= Math.ceil(client.guilds.cache.size / 10)) return
+  
+              i0 = i0 + 10
+              i1 = i1 + 10
+              page = page + 1
+  
+              let description2 = 
+              `Total Servers - ${client.guilds.cache.size}\n\n` +
+              client.guilds.cache
+                .sort((a, b) => b.memberCount - a.memberCount)
+                .map(r => r)
+                .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
+                .slice(i0, i1)
+                .join("\n\n");
+  
+              const embed1 = new MessageEmbed()
+              .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
+              .setColor(color)
+              .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
+              .setDescription(description2);
+  
+              return await msg.edit({ embeds: [embed1] })
+          }
+        })
         }
-
-        if(b.customId === 'right'){
-          if(page >= Math.ceil(client.guilds.cache.size / 10)) return
-
-            i0 = i0 + 10
-            i1 = i1 + 10
-            page = page + 1
-
-            let description2 = 
-            `Total Servers - ${client.guilds.cache.size}\n\n` +
-            client.guilds.cache
-              .sort((a, b) => b.memberCount - a.memberCount)
-              .map(r => r)
-              .map((r, i) => `**${i + 1}** - ${r.name} \n **User:** ${r.members.cache.filter(m => !m.user.bot).size} | **Bot:** ${r.members.cache.filter(m => m.user.bot).size} | **Total Members:** ${r.memberCount}\nID - ${r.id}`)
-              .slice(i0, i1)
-              .join("\n\n");
-
-            const embed1 = new MessageEmbed()
-            .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic : true}))
-            .setColor(color)
-            .setFooter(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
-            .setDescription(description2);
-
-            return await msg.edit({ embeds: [embed1] })
-        }
-      })
     }
 }
